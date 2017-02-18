ce0538485178ebcb8900d737b4b94eb5668a8926
Dino Rodriguez <dinorodriguez@college.harvard.edu>
2017-02-18 15:29:23 -0500

    graph.h

---
+++ Programming Assignment 1/include/graph.h
@@ -0,0 +1,23 @@
+/*
+ * Header file for graph structure
+ *
+ * Made by SJ Kim and Dino Rodriguez.
+ */
+
+
+// Class for complete and undirected graphs
+class Complete_Undirected {
+
+private:
+    vertices;
+
+public:
+    // constructor, always takes in vertices
+    Complete_Undirected( vertices ) {
+        std::cout << "Complete_Undirected constructor called." << '\n';
+    }
+    // destructor, implicitly called and no arguments
+    ~Complete_Undirected( void ) {
+        std::cout << "Complete_Undirected constructor called." << '\n';
+    }
+}
