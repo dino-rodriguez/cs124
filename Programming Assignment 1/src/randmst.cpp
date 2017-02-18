9956c6751fdc90730c40b9b7842bdf08da8fc75a
Dino Rodriguez <dinorodriguez@college.harvard.edu>
2017-02-18 15:00:13 -0500

    makefile done

--- Programming Assignment 1/src/randmst.cpp
+++ Programming Assignment 1/src/randmst.cpp
@@ -1,7 +1,5 @@
 #include <fstream>
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 
 using namespace std;
 
@@ -9,14 +7,10 @@ int main(int argc, char *argv[])
 {
 	// Checks number of arguments
 	if (argc != 5) 
-		cout<<"Usage: "<< argv[0] <<" 0 numpoints numtrials dimension\n";
+		cout<<"usage: "<< argv[0] <<" 0 numpoints numtrials dimension\n";
 	else 
 		for ( int i = 1; i < argc; i++ ) {
-			int n;
-			argv[i] >> n;
-			if (!argv[i].fail()) {
-				cout<<"Fail";
-			}
+			cout << "value of a: " << argv[i] << endl;
 		}
 	return 0;
 }
