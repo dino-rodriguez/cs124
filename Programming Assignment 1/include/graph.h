2aecb2ba8c77d426590f9f29d4d5236460fee49a
Dino Rodriguez <dinorodriguez@college.harvard.edu>
2017-02-18 15:41:09 -0500

    Initial class signature complete

--- Programming Assignment 1/include/graph.h
+++ Programming Assignment 1/include/graph.h
@@ -9,15 +9,19 @@
 class Complete_Undirected {
 
 private:
-    vertices;
+    int vertices, dimension;
 
 public:
     // constructor, always takes in vertices
-    Complete_Undirected( vertices ) {
+    Complete_Undirected(vertices, dimension) {
         std::cout << "Complete_Undirected constructor called." << '\n';
     }
     // destructor, implicitly called and no arguments
     ~Complete_Undirected( void ) {
         std::cout << "Complete_Undirected constructor called." << '\n';
     }
+
+    // methods
+    void generate_graph(int, int);
+    void print_graph();
 }
