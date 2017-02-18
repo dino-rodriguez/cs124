6d8036e99bb338f4af4775ad140277f0116503f2
Seon Joong Kim <seonjoongkim@college.harvard.edu>
2017-02-18 16:53:22 -0500

    graph work

--- Programming Assignment 1/src/complete_undirected.cpp
+++ Programming Assignment 1/src/complete_undirected.cpp
@@ -1,23 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 
 using namespace std;
 
 struct Node {
-	int p;
+	int val;
 	struct Node* next;
-}
+};
 
 struct AList {
 	struct Node* head;
-}
+};
 
-struct cuGraph {
+class Graph {
 	private: 
-		int Vertices;
+		int V;
 		struct AList* lst;
-}
+	public: 
+		Graph(int Vert) {
+			this->V = V;
+			lst = new AList [Vert];
+			for (int i = 0; i < Vert; i++)
+				lst[i].head = NULL;
 
-struct Node* createNode(int p) {
-	
-}
+		}
+		Node* createNode(int val) {
+			Node* node = new Node;
+			node->val = val;
+			node->next = NULL;
+			return node;
+		}
+		void createEdge(int src, int sink) {
+			Node* node = createNode(sink);
+			node->next = lst[src].head;
+			lst[src].head = node;
+			node = createNode(src);
+			node->next = lst[sink].head;
+			lst[sink].head = node;
+		}
+
+};
+
+// int main(void) {
+// 	Graph  
+// }
+
