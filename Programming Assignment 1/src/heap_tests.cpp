7401ed19492e6998e959060cfbe22c95d980c102
Seon Joong Kim <seonjoongkim@college.harvard.edu>
2017-02-19 22:34:26 -0500

    heap broken

--- Programming Assignment 1/src/heap_tests.cpp
+++ Programming Assignment 1/src/heap_tests.cpp
@@ -10,8 +10,8 @@
 using namespace std;
 
 int main(void) {
-	int A[4] = {4, 1, 2, 3};
-    Heap H = Heap(4, A);  
+	int A[6] = {10, 9, 8, 6, 3, 1};
+    Heap H = Heap(6, A);  
     H.build_heap();
     // cout<<"Extracting Min"<<'\n';
     // int min = H.extract_min();
