683933b373c6f36c132b310a8063b61f5499b349
Seon Joong Kim <seonjoongkim@college.harvard.edu>
2017-02-19 21:43:36 -0500

    heap in progress

--- Programming Assignment 1/include/heap.h
+++ Programming Assignment 1/include/heap.h
@@ -24,61 +24,91 @@ private:
 
 public:
     // constructor, always takes in vertices
-    Heap(int s = 0, int* arr) : sz(s), H(arr) {
+    Heap(int s = 0, int* arr = 0) : sz(s), H(arr) {
         std::cout << "Heap constructor called." << '\n';
     }
     // destructor, implicitly called and no arguments
     ~Heap(void) {
         std::cout << "Heap deconstructor called." << '\n';
     }
-    int findParent(int i);
+
+    // methods
+    int Parent(int i);
     int Left(int i);
     int Right(int i);
-    // SJ
-    void minHeapify();
-    void buildHeap();
-
-    // Dino
-    void peak();
-    void extractMax();
-    void insert();
+    void min_heapify(int i);
+    void build_heap();
+    void print_heap();
+    void print_size();
+    int peek();
+    int extract_min();
 };
 
-int Parent(int i) {
+int Heap::Parent(int i) {
     return floor(i / 2);
 }
 
-int Left(int i) {
+int Heap::Left(int i) {
     return 2*i;
 }
 
-int Right(int i) {
+int Heap::Right(int i) {
     return 2*i + 1;
 }
 
 // ensure that i is the root of the max heap
-void minHeapify(int i) {
+void Heap::min_heapify(int i) {
     int smallest;
-    int l = Left(i);
-    int r = Right(i);
-    if (l < sz && this->H[l] < this->H[i])
+    int l = this->Left(i);
+    int r = this->Right(i);
+    if (l < this->sz && this->H[l] < this->H[i])
         smallest = l;
     else 
         smallest = i;
-    if (r < sz && this->H[r] < this->H[smallest])
+    if (r < this->sz && this->H[r] < this->H[smallest])
         smallest = r;
     if (smallest != i) {
+        cout<<"swap!"<<'\n';
         int buf = this->H[i];
         this->H[i] = this->H[smallest];
         this->H[smallest] = buf;
-        minHeapify(smallest);
+        this->min_heapify(smallest);
     }
 }
 
-void buildHeap() {
-    for (int k = floor(sz/2); i > 1; i--) {
-        minHeapify(k);
+void Heap::build_heap() {
+    for (int k = floor(this->sz/2); k > 1; k--) {      
+        this->min_heapify(k);
     }
+}
+
+void Heap::print_heap() {
+    for (int i = 0; i < this->sz; i++) {
+        cout<<this->H[i]<<',';
+    }
+    cout<<'\n';
+}
+
+void Heap::print_size() {
+    cout<<this->sz<<'\n';
+}
+
+int Heap::peek() {
+    if (this->sz > 0)
+        return this->H[0];
+    // if nothing is in the heap
+    else 
+        return -1;
+}
+
+int Heap::extract_min() {
+    // there must be at least one element on the heap
+    assert(this->sz > 0);
+    int min = this->H[0];
+    this->H[0] = this->H[this->sz - 1];
+    this->sz -= 1;
+    this->min_heapify(0);
+    return min;
 }
 
 
