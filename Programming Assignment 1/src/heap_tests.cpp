4e8eaa7ede81754c1b0e9945a8d12bab605b3136
Seon Joong Kim <seonjoongkim@college.harvard.edu>
2017-02-26 17:06:04 -0500

    heap works

--- Programming Assignment 1/src/heap_tests.cpp
+++ Programming Assignment 1/src/heap_tests.cpp
@@ -11,16 +11,35 @@ using namespace std;
 
 int main(void) {
 	Heap H = Heap();
-	H.insert(5);
-	H.insert(1);
-	H.insert(4);
-	H.insert(2);
-	H.insert(3);
-	H.print();
-	for (int i = 0; i < 5; i++) {
-		int min = H.extract_min();
-		cout<<min<<'\n';
+	
+	entry A;
+	A.vertex = 1;
+	A.dist = 12;
+
+	entry B;
+	B.vertex = 2;
+	B.dist = 5;
+
+	entry C;
+	C.vertex = 3;
+	C.dist = 7;
+
+	entry D;
+	D.vertex = 4;
+	D.dist = 19;
+
+	H.insert(A);
+	H.insert(B);
+	H.insert(C);
+	H.insert(D);
+
+	int sz = H.get_size();
+	for (int i = 0; i < sz; i++) {
+		entry min = H.delete_min();
+		cout<<"Vertex: "<<min.vertex<<'\n'<<"Value: "<<min.dist<<"\n\n";
 	}
+
+
 }
 
 
