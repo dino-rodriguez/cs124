298bba7f5cbbabb33e22483704128f82d4f300ea
Seon Joong Kim <seonjoongkim@college.harvard.edu>
2017-02-26 17:09:39 -0500

    fin heap

--- Programming Assignment 1/include/heap.h
+++ Programming Assignment 1/include/heap.h
@@ -10,103 +10,100 @@
 #include <iostream>
 #include <time.h>
 #include <math.h>
+#include <vector>
 
 using namespace std;
+
+struct entry {
+    int vertex;
+    int dist;
+};
 
 // class for complete and undirected graphs
 class Heap {
 
 private:
     // variables for number of vertices and dimension
-    int sz;
-    int* H;
+    vector<entry> H;
+    int Parent(int i);
+    int Left(int i);
+    int Right(int i);
+    void swap(int a, int b);
+    void heap_down(int i);
+    void heap_up(int i);
 
 public:
     // constructor, always takes in vertices
-    Heap(int s = 0, int* arr = 0) : sz(s), H(arr) {
+    Heap(void) {
         std::cout << "Heap constructor called." << '\n';
     }
     // destructor, implicitly called and no arguments
     ~Heap(void) {
         std::cout << "Heap deconstructor called." << '\n';
     }
+    int get_size() {
+        return H.size();
+    }
+    void insert(entry i);
+    entry delete_min();
 
-    // methods
-    int Parent(int i);
-    int Left(int i);
-    int Right(int i);
-    void min_heapify(int i);
-    void build_heap();
-    void print_heap();
-    void print_size();
-    int peek();
-    int extract_min();
 };
 
 int Heap::Parent(int i) {
-    return floor(i/ 2) - 1;
+    return (i - 1)/2;
 }
 
 int Heap::Left(int i) {
-    return 2*i;
+    return 2*i + 1;
+
 }
 
 int Heap::Right(int i) {
-    return 2*i + 1;
+    return 2*i + 2;
 }
 
-// ensure that i is the root of the max heap
-void Heap::min_heapify(int i) {
-    int smallest;
-    int l = this->Left(i);
-    int r = this->Right(i);
-    if (l < this->sz && this->H[l] < this->H[i])
-        smallest = l;
-    else
-        smallest = i;
-    if (r < this->sz && this->H[r] < this->H[smallest])
-        smallest = r;
-    if (smallest != i) {
-        int buf = this->H[i];
-        this->H[i] = this->H[smallest];
-        this->H[smallest] = buf;
-        this->min_heapify(smallest);
+void Heap::swap(int a, int b) {
+    entry temp = H[a];
+    H[a] = H[b];
+    H[b] = temp;
+}
+
+void Heap::heap_down(int i) {
+    int l = Left(i);
+    int r = Right(i);
+    int small = i;
+
+    if (l < get_size() && H[l].dist < H[i].dist) 
+        small = l;
+    if (r < get_size() && H[r].dist < H[small].dist)
+        small = r;
+    if (small != i) {
+        swap(i, small);
+        heap_down(small);
     }
 }
 
-void Heap::build_heap() {
-    for (int k = floor((this->sz)/2); k >= 1; k--) {
-        this->min_heapify(k-1);
-    }
+void Heap::heap_up(int i) {
+    int p = Parent(i);
+    if (i && H[p].dist > H[i].dist) {
+        swap(i, p);
+        heap_up(p);
+    } 
 }
 
-void Heap::print_heap() {
-    for (int i = 0; i < this->sz; i++) {
-        cout<<this->H[i]<<',';
-    }
-    cout<<'\n';
+void Heap::insert(entry i) {
+    H.push_back(i);
+    heap_up(get_size() -1);
 }
 
-void Heap::print_size() {
-    cout<<this->sz<<'\n';
-}
-
-int Heap::peek() {
-    if (this->sz > 0)
-        return this->H[0];
-    // if nothing is in the heap
-    else
-        return -1;
-}
-
-int Heap::extract_min() {
-    // there must be at least one element on the heap
-    assert(this->sz > 0);
-    int min = this->H[0];
-
-    // swap the root with the last element
-    this->H[0] = this->H[this->sz - 1];
-    this->sz -= 1;
-    this->min_heapify(0);
+entry Heap::delete_min() {
+    int sz = get_size();
+    assert(sz > 0);
+    entry min = H[0];
+    H[0] = H.back();
+    H.pop_back();
+    heap_down(0);
     return min;
+
 }
+
