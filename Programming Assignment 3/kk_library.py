af7d8a32f4d8c4e69d2a6f618b5f8b906649c0fb
Colton Gyulay <coltongyulay@gmail.com>
2017-04-15 20:02:35 -0400

    algos seem to work, need better test coverage and code review

--- Programming Assignment 3/kk_library.py
+++ Programming Assignment 3/kk_library.py
@@ -5,14 +5,14 @@
 
 # libraries
 import random
+import math
 
 
 # Generate a random set of numbers
     # s : size of output set
     # flag : int for testing
 def randomSet(s, flag):
-
-    random.seed() # seed random number generator
+    random.seed()
     A = []
 
     # generate list
@@ -24,30 +24,22 @@ def randomSet(s, flag):
     return A
 
 
-# Prepartition a set A into a partition set
-    # A : set of integers
-def prepartition(A):
-
-    # TODO
-    return 0
-
-
 # Karmarkar-Karp algorithm, differencing approach
     # A : set of integers
     # flag : int for testing
-def difKK(A, flag):
-
+def diffKK(A, flag):
     # make sure set is big enough
-    if len(A) <= 2:
+    A1 = A[:]
+    if len(A1) <= 2:
         return -1
 
-    if flag == 1: print A
+    if flag == 1: print A1
 
     # get two max elements
-    a_i = max(A)
-    A.remove(a_i)
-    a_j = max(A)
-    A.remove(a_j)
+    a_i = max(A1)
+    A1.remove(a_i)
+    a_j = max(A1)
+    A1.remove(a_j)
 
     difference = abs(a_i - a_j) # difference of two elements
 
@@ -55,32 +47,168 @@ def difKK(A, flag):
     while a_i != 0 and a_j != 0:
 
         # insert new elements into list
-        A.append(difference)
-        A.append(0)
+        A1.append(difference)
+        A1.append(0)
 
-        if flag == 1: print A
+        if flag == 1: print A1
 
         # get two max elements
-        a_i = max(A)
-        A.remove(a_i)
-        a_j = max(A)
-        A.remove(a_j)
+        a_i = max(A1)
+        A1.remove(a_i)
+        a_j = max(A1)
+        A1.remove(a_j)
 
         difference = abs(a_i - a_j) # difference of two elements
 
     return difference
 
 
-# Repeatedly generate random solutions to the problem,
-# as determined by the representation
-    # P : set of integers after partitioning
-    # flag : int for testing
+# Return a random prepartitioning representation for a set of numbers A
+    # A : set of integers
+def randomPrepartitionRep(A):
+    n = len(A)
+    return [random.choice(range(1, n + 1)) for i in range(n)]
+
+
+# Return a random standard representation for a set of numbers A
+    # A : set of integers
+def randomStandardRep(A):
+    return [random.choice([-1, 1]) for i in range(len(A))]
+
+
+# Calculate the residue for a set of numbers A while enforcing prepartioning
+# representation P
+    # A : set of integers
+    # P : set of integers over [1, n]
+def prepartitionResidue(A, P):
+    assert(len(A) == len(P))
+
+    # first convert to KK digestible format
+    n = len(A)
+    A1 = [0] * n
+    for i, p in enumerate(P):
+        A1[p - 1] += A[i]
+
+    # return value from KK
+    return abs(diffKK(A1, 0))
+
+
+# Calculate the residue for a set of numbers A while enforcing standard
+# representation S
+    # A : set of integers
+    # S : set of integers, composed only of -1 or 1
+def standardResidue(A, S):
+    assert(len(A) == len(S))
+
+    # sum of a_i * s_i
+    return abs(sum([a * s for a, s in zip(A, S)]))
+
+
+# Calculate a random 'neighbor' for a prepartitioning representation P
+    # P : set of integers over [1, n]
+def prepartitionNeighbor(P):
+    P1 = P[:]
+    n = len(P1)
+    i, j = random.sample(range(1, n + 1), 2)
+    i -= 1 # offset index by 1 as we're sampling from [1, n] inclusive
+    P1[i] = j
+
+    return P1
+
+
+# Calculate a random 'neighbor' for a standard representation S
+    # S : set of integers, composed only of -1 or 1
+def standardNeighbor(S):
+    S1 = S[:]
+    n = len(S1)
+    i, j = random.sample(S1, 2)
+
+    # switch element i
+    S1[i] = -S1[i]
+
+    # with prob 1/2, switch element j
+    if random.uniform(0, 1) < 0.5:
+        S1[j] = -S1[j]
+
+    return S1
+
+
+# Helper method returns corresponding generation, residue, and neighbor
+# functions for a specific representation type type_
+    # type_ : string to denote representation type, 's' or 'p'
+def gatherFns(type_):
+    if type_ == 's':
+        gen_fn = randomStandardRep
+        residue_fn = standardResidue
+        neighbor_fn = standardNeighbor
+    elif type_ == 'p':
+        gen_fn = randomPrepartitionRep
+        residue_fn = prepartitionResidue
+        neighbor_fn = prepartitionNeighbor
+    else:
+        raise ValueError("Invalid representation type, must be 'p' or 's'")
+
+    return gen_fn, residue_fn, neighbor_fn
+
+
+# Repeatedly generate random solutions to the problem, as determined by the
+# representation
+    # A : set of integers to partition
     # max_iter : int to denote number of times to generate solution
-    # n : size of solution sequence
-def repRandom(S, n, max_iter, flag):
+    # type_ : string to denote representation type, 's' or 'p'
+    # flag : int for testing
+def repRandom(A, max_iter, type_, flag):
+    gen, residue, neighbor = gatherFns(type_)
+    S = gen(A)
 
-    for 1 to range(max_iter):
-        S1 = randomSet(s)
-        if difKK(S1, 0) < difKK(S, 0): S = S1
+    for i in range(max_iter):
+        S1 = gen(A)
+        if residue(A, S1) < residue(A, S):
+            S = S1
 
-    return S
+    return residue(A, S)
+
+
+# Generate a random solution, then attempt to improve it through moves to
+# better neighbors
+    # A : set of integers to partition
+    # max_iter : int to denote number of times to generate solution
+    # type_ : string to denote representation type, 's' or 'p'
+    # flag : int for testing
+def hillClimb(A, max_iter, type_, flag):
+    gen, residue, neighbor = gatherFns(type_)
+    S = gen(A)
+
+    for i in range(max_iter):
+        S1 = neighbor(S)
+        if residue(A, S1) < residue(A, S):
+            S = S1
+
+    return residue(A, S)
+
+
+# Generate a random solution, then attempt to improve it through moves to
+# neighbors that are not always better
+    # A : set of integers to partition
+    # max_iter : int to denote number of times to generate solution
+    # type_ : string to denote representation type, 's' or 'p'
+    # flag : int for testing
+def simAnneal(A, max_iter, type_, flag):
+    gen, residue, neighbor = gatherFns(type_)
+    S = gen(A)
+    S11 = S[:]
+
+    def T(i):
+        return (10 ** 10) * (0.8 ** (i // 300))
+
+    for i in range(max_iter):
+        S1 = neighbor(S)
+        if residue(A, S1) < residue(A, S):
+            S = S1
+        # TODO: ensure probability to switch to worse neighbor is correct
+        elif random.uniform(0, 1) < math.exp(-(residue(A, S1) - residue(A, S)) / T(i)):
+            S = S1
+        if residue(A, S) < residue(A, S11):
+            S11 = S
+
+    return residue(A, S11)
--- Programming Assignment 3/kk_test.py
+++ Programming Assignment 3/kk_test.py
@@ -8,13 +8,51 @@
 import kk_library as KK
 import kk as Main
 
+
 def randomSetTest():
     return KK.randomSet(100, not Main.flag)
 
-def difKKTest():
+
+def diffKKTest():
     A = [10, 8, 7, 6, 5]
-    residue = KK.difKK(A, not Main.flag)
+    residue = KK.diffKK(A, not Main.flag)
+
+
+def prepartitionTest():
+    A = [10, 8, 7, 6, 5]
+    P = [1, 2, 2, 4, 5]
+    residue = KK.prepartitionResidue(A, P)
+
+
+def algoTest():
+    # TODO: timing and avgs across trials for all algorithms
+
+    for i in range(1): # switch to 100 for actual trials
+        max_iter = 1000 # use 25000 for actual trials
+        A = KK.randomSet(100, not Main.flag)
+        kk_res = KK.diffKK(A, not Main.flag)
+
+        s_rand_res = KK.repRandom(A, max_iter, 's', not Main.flag)
+        s_hill_res = KK.hillClimb(A, max_iter, 's', not Main.flag)
+        s_anneal_res = KK.simAnneal(A, max_iter, 's', not Main.flag)
+
+        p_rand_res = KK.repRandom(A, max_iter, 'p', not Main.flag)
+        p_hill_res = KK.hillClimb(A, max_iter, 'p', not Main.flag)
+        p_anneal_res = KK.simAnneal(A, max_iter, 'p', not Main.flag)
+
+        print 'KK: {}'.format(kk_res)
+
+        print 'Standard representation repeated random: {}'.format(s_rand_res)
+        print 'Standard representation hill climbing: {}'.format(s_hill_res)
+        print 'Standard representation simulated annealing: {}'.format(s_anneal_res)
+
+        print 'Prepartioning representation repeated random: {}'.format(p_rand_res)
+        print 'Prepartioning representation hill climbing: {}'.format(p_hill_res)
+        print 'Prepartioning representation simulated annealing: {}'.format(p_anneal_res)
+
 
 # running tests
 randomSetTest()
-difKKTest()
+diffKKTest()
+prepartitionTest()
+algoTest()
